d3e9c5f5efcc74dc035248f89bd041c7432a39a1
Abdur Rahman tumzied <50454992+tz01x@users.noreply.github.com>
2019-10-18 06:47:46 +0000

    Create test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+#from excel2json import convert_from_file
+
+
+#EXCEL_FILE = 'file_example_XLSX_10.xlsx'  # or '../example.xlsx'
+#convert_from_file(EXCEL_FILE)
+
+
+import json
+f=open('Sheet1.json')
+with open('Sheet1.json') as jsonfile:
+	data=json.load(jsonfile)
+	for p in data:
+		print(p["First Name"])
